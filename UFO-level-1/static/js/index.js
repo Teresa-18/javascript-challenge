5b793a9da97627af079a83f5dca84c013a4fc9e3
teresa luna <tmluna_18@outlook.com>
2021-01-23 15:23:21 -0600

    adding images to image folder and adding some button and form control js code

--- UFO-level-1/static/js/index.js
+++ UFO-level-1/static/js/index.js
@@ -1,3 +1,4 @@
+
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
@@ -16,3 +17,37 @@ data.forEach((ufoSighting) => {
         cell.text(value);
     });
 });
+
+// from data.js
+var tableData = data;
+
+// Select the button
+var button = d3.select("#button");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers for clicking the button or pressing the enter key
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Create the function to run for both events
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  // Print the value to the console
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter(table => table.datetime === inputValue);
+
+  console.log(filteredData);
+};
