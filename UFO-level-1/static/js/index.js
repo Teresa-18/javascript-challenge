a1a086305497480e6a7b73360f3e4fce03f93db3
teresa luna <tmluna_18@outlook.com>
2021-01-23 23:51:04 -0600

    adding code that got filtered results to show in Website

--- UFO-level-1/static/js/index.js
+++ UFO-level-1/static/js/index.js
@@ -1,6 +1,7 @@
 var tableData = data;
 // Get a reference to the table body
 var tbody = d3.select("tbody");
+
 data.forEach((ufoSighting) => {
   var row = tbody.append("tr");
   Object.entries(ufoSighting).forEach(([key, value]) => {
@@ -11,34 +12,28 @@ data.forEach((ufoSighting) => {
 
 // // from data.js
 // var sightings = data;
-
-// Select the button
-// var button = d3.select("#filter-btn");
-// Select the form
-// var form = d3.select("#form");
-
 // Create event handlers for clicking the button or pressing the enter key
-d3.select("#filter-btn").on("click", runEnter);
-// d3.select("#form").on("submit", runEnter);
+var button = d3.select("#filter-btn");
+var form = d3.select("#form");
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
 
 // Create the function to run for both events
 function runEnter() {
-tbody.html("");
-  // d3.event.preventDefault();
+  tbody.html("");
+  d3.event.preventDefault();
   var inputValue = d3.select("#datetime").property("value");
   var filteredData = tableData;
   if (inputValue != "") {
-    filteredData = filteredData.filter(r => r.datetime == inputValue)
+    filteredData = filteredData.filter(r => r.datetime == inputValue);
+    console.log(filteredData);
   }
-  // var tbody = d3.select("tbody");
-  for (var i = 0; i < filteredData.length; i ++) {
-    data.forEach((filteredData) => {
+  filteredData.forEach((ufoSighting) => {
       var row = tbody.append("tr");
-      row.append ("td").text(entry.datetime)
-      // Object.entries(filteredData).forEach(([key, value]) => {
-      //   var cell = row.append("td");
-      //   cell.text(value);
+      Object.entries(ufoSighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
       });
     });
-  }
 }
